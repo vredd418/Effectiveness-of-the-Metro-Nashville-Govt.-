42d618eb9fb340e6c7775006ccf9709c8c130e16
vredd418 <vredd418@gmail.com>
2022-01-11 21:29:46 -0600

    finished cleaning and merging data

--- global.R
+++ global.R
@@ -5,6 +5,9 @@ library(lubridate)
 library(rgdal)
 library(ggplot2)
 library(tidycensus)
+library(sf)
+library(tigris)
+census_api_key("86875983b50f2fae4cdb9463bafa4f584c31b2f8")
 
 df = read.socrata("https://data.nashville.gov/resource/7qhx-rexh.json")
 df <- df %>% subset(., select = c("status", "case_request", "case_subrequest", "additional_subrequest", "date_time_opened",
@@ -14,10 +17,21 @@ df <- df %>% filter(year(date_time_opened) == 2019) %>%
   filter_at(vars(mapped_location.latitude, mapped_location.longitude), all_vars(!is.na(.)))
 
 tract <- readOGR(dsn = "C:/Users/vredd/Documents/data_science_projects/Midcourse project/Effectiveness-of-the-Metro-Nashville-Govt.-/data",
-                layer = "tl_2019_47_tract")
+                layer = "tl_2019_47_tract") %>% 
+  subset(., COUNTYFP %in% "037")
 
-census <- get_acs(geography = "census",
-                  variables = ,
-                  year = 2019)
+census <- get_acs(geography = "tract",
+                  variables = c(median_income = "B06011_001", population = "B01001_001",
+                                white = "B01001H_001", asian = "C02003_006", hispanic = "B01001I_001",
+                                black = "C02003_004", median_age = "B01002_001"),
+                  state = "TN",
+                  county = 37,
+                  year = 2019) %>% 
+  select(., -c(moe)) %>% 
+  spread(., key = variable, value = estimate)
 
-vars <- load_variables(2019, "acs5", cache = TRUE)
+
+tract_census <- geo_join(tract, census, by = "GEOID") %>% 
+  select(., -c(GEOID.1, NAME.1))
+
+
